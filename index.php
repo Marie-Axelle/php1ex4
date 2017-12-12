a5c6723af0e4621d6847230b5c2bee54e08db049
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-12 10:39:45 +0100

    differente variables

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$string="Je suis chiante et j'assume.\n";
+echo $string;
+$kg= 12;
+echo "Ma conso de clementine par jour : $kg.\n";
+$km= 1756;
+echo "Mira est à $km km.\n";
+$je_suis_grande= false;
+$je_suis_bornée= true;
+echo $je_suis_bornée;
+ ?>
